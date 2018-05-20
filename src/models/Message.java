6d1538e79d27c4925f692e3408178528f75ca0e4
jun.tamura <cornflakesplain_0310@ezweb.ne.jp>
2018-05-20 21:58:37 +0900

    Add NewServret.java

--- src/models/Message.java
+++ src/models/Message.java
@@ -33,7 +33,7 @@ public class Message {
     private String content;
     
     @Column(name = "created_at", nullable = false)
-    private String created_at;
+    private Timestamp created_at;
     
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
@@ -62,11 +62,11 @@ public class Message {
         this.content = content;
     }
 
-    public String getCreated_at() {
+    public Timestamp getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(String created_at) {
+    public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
 
